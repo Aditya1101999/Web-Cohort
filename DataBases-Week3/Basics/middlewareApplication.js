9678547d6a331d654d1a642d181a6b86da5280c4
Aditya Choudhary <adi854105@gmail.com>
2023-12-29 02:47:24 +0530

    middlewares assignment done

--- DataBases-Week3/Basics/middlewareApplication.js
+++ DataBases-Week3/Basics/middlewareApplication.js
@@ -3,7 +3,18 @@ const express=require("express")
 const app =express()
 
 app.use(express.json())
-app.get("/ride1",(req,res)=>{
+
+function middleware(req,res,next){
+    console.log(`Request made to ${req.url}`)
+    next()
+}
+//only triggers for the route handlers below this
+app.use((req,res,next)=>{
+    const age=req.query.age
+    if(age>=14) next()
+    else res.json({msg : "go home kiddo"})
+})
+app.get("/ride1", middleware, (req,res)=>{
     res.json({
         msg:"riden ride 1"
     })
